c1b52c70be03cd5c61a84ff19f5af50dd42ee386
Therealkhalil <khalil.chabir96@gmail.com>
2023-01-04 02:51:46 +0100

    redifine script(optimized) and manage level 2 design

---
+++ Cubic Maze/Assets/Script/playermvt.cs
@@ -0,0 +1,55 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playermvt : MonoBehaviour
+{
+    private Rigidbody _rb;
+    private float inputx, inputz,firstinputx,firstinputz;
+    [SerializeField] private float speed = 10f;
+    private bool moving = false;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _rb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        inputx = Input.GetAxisRaw("Horizontal");
+        inputz = Input.GetAxisRaw("Vertical");
+        movement();
+    }
+
+
+    void movement()
+    {
+        if (inputx != 0 && moving == false)
+        {
+            firstinputx = inputx;
+            moving = true;
+        }
+        else if(inputz != 0 && moving == false)
+        {
+            firstinputz = inputz;
+            moving = true;
+        }
+
+        if (moving)
+            _rb.velocity = new Vector3(firstinputx, 0f, firstinputz) * speed;
+    }
+
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if(collision.gameObject.tag == "Block")
+        {
+            moving = false;
+            firstinputx = 0f;
+            firstinputz = 0f;
+        }
+    }
+}
